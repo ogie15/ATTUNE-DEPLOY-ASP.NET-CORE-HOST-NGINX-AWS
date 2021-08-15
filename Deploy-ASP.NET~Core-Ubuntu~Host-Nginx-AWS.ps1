442de2f43753683027192c577673b74e248d5097
Okougbo Ibhadogiemu <iokougbo@gmail.com>
2021-08-15 19:07:28 +0100

    file updated

---
+++ Deploy-ASP.NET~Core-Ubuntu~Host-Nginx-AWS.ps1
@@ -0,0 +1,112 @@
+#Region for ExecutionPolicy
+# ===========================================================================
+# Get Execution Policy of the current process
+$Script:ProcessEP = Get-ExecutionPolicy -Scope Process
+
+#Get the value of the Execution Policy and save it in the Variable
+$Script:ValueProcessEP = ($Script:ProcessEP).value__
+
+# Check if the Execution Policy of the process is set to Unrestricted
+if ($Script:ValueProcessEP -eq 0) {
+
+    # echo the message
+    Write-Output "Execution Policy is already set to Unrestricted for the Process"
+# Check if the Execution Policy of the process is already set
+}else{
+
+    # Set the ExecutionPolicy of the Process to Unrestricted
+    Set-ExecutionPolicy -Scope Process -ExecutionPolicy Unrestricted -Force -Confirm:$false
+
+    # Checks if the Execution Policy has been set
+    if ((Get-ExecutionPolicy -Scope Process).value__ -eq 0) {
+
+        # echo the message
+        Write-Output "Execution Policy is now set to Unrestricted for the Process"
+    }
+}
+# ===========================================================================
+#EndRegion for ExecutionPolicy 
+
+
+
+
+#Region Start EC2 Instance
+# ===========================================================================
+# Import Module for AWS PowerShell
+Import-Module -Name AWSPowerShell
+
+# # Save accesskey to this Variable
+$Script:AccessKeyValue = "{accesskey.value}"
+
+# # Save secretkey to this variable
+$Script:SecretKeyValue = "{secretkey.value}"
+
+# # Set value to store profile 
+$Script:ProfileNameVaule = "DefaultSetKeys"
+
+# Set the AWS Image ID
+$Script:AWSImageId = "ami-0244a5621d426859b"
+
+# Set the AWS Instance Type
+$Script:AWSInstanceType = "t2.micro"
+
+# # Hash Table of InstanceId with coressponding region pair
+# $Script:HashValue = {hashvalue.value}
+$Script:HashValue = @{"Region" = "eu-west-2";"KeyPair" = "pemkey"}
+
+
+
+# UserData script for DotNet and Nginx
+$Script:UserDataText = "#!/bin/bash
+wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update; \
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get install -y dotnet-sdk-5.0
+sudo apt-get install -y dotnet-sdk-3.1
+sudo apt-get install -y apt-transport-https && \
+sudo apt-get install -y aspnetcore-runtime-5.0
+sudo apt-get install -y aspnetcore-runtime-3.1
+sudo apt-get install -y nginx"
+
+
+
+# Set AWS Credentials
+Set-AWSCredential -AccessKey $Script:AccessKeyValue -SecretKey $Script:SecretKeyValue -StoreAs $Script:ProfileNameVaule
+
+
+
+# check Hash Table
+if ($null -eq $Script:HashValue['KeyPair'] -or $null -eq $Script:HashValue['Region']) {
+
+    # echo the message
+    Write-Output "Please check the Hash Table"
+}else {
+
+    # echo the message
+    Write-Output "All keys are present in the Hash Table... checking keypair"
+
+    # Checking KeyPair in hashtable
+    if (!($Script:HashValue.ContainsKey('KeyPair')) -or $Script:HashValue.KeyPair -like "") {
+
+        # echo the message
+        Write-Output "KeyPair does not exist in the Hash Table or it is an empty string"
+
+    }else {
+
+        # echo the message
+        Write-Output "Creating Ubuntu EC2 instance with NGINX and DOTNET Core"
+
+        # Creat New EC@2 Instance
+        New-EC2Instance -ImageId $Script:AWSImageId -InstanceType $Script:AWSInstanceType -KeyName $Script:HashValue['KeyPair'] -Region $Script:HashValue['Region']`
+        -ProfileName $Script:ProfileNameVaule -UserData $Script:UserDataText -EncodeUserData -Force
+    }
+}
+
+
+
+# Remove Profile
+Remove-AWSCredentialProfile -ProfileName $Script:ProfileNameVaule -Force
+
+#EndRegion Start EC2 Instance
+# ===========================================================================
---
+++ README.md
@@ -0,0 +1,5 @@
+# **Deploy~ASP.NET~CORE-Ubuntu~Host-Nginx-AWS Blueprint**
+***This is my repo Blueprint Source Code on Attune for deploying an Ubuntu AWS EC2 instance with NGINX and DotNet CORE.***
+---
+[![SERVERTRIBE](https://www.servertribe.com/wp-content/themes/mars/assets/images/attune_logo.svg)](https://www.servertribe.com/)
+***&copy;2021 Powered by ServerTribe***
